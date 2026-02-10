db961826763b6a43e2839b22782ec19ab477b3e1
mohitrocks001 <mohitrocks001@gmail.com>
2026-02-09 18:48:12 -0800

    Update index.tsx

--- Globalimage/index.tsx
+++ Globalimage/index.tsx
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const rootElement = document.getElementById('root')
-
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to")
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
